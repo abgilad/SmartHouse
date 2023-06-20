80519ef77cab2fa06881e995db5f5eed2515eb10
abgilad <120219883+abgilad@users.noreply.github.com>
2023-06-20 09:22:24 +0300

    20.06.23 update

--- newapp/src/App.js
+++ newapp/src/App.js
@@ -9,16 +9,35 @@ import ChooseNewRoom from './Components/ChooseNewRoom';
 
 function App() {
 
-  const [data, setData] = useState([])
+  const [data, setData] = useState([{choosenRoom:'kitchen',roomName:'Gilad',color:'red'}])
+  console.log(data);
 
+  //Functions////////////////////////////////
 
+  const addNewRoom = (chooseRoom,name,color)=>{
+    const findTheSameRoom = data.find((val)=>val.roomName===name);
+    
+     if(findTheSameRoom){
+      return alert('the room name is exist!')
+     }
+     else if(!(findTheSameRoom)){
+      let temp= {
+        choosenRoom:chooseRoom,
+        roomName:name,
+        color
+      }
+      setData([...data,temp])
+     }
+  }
+
+  
 
   return (
     <div className="App">
       <Logo />
       <Routes>
         <Route path='/' element={<CreateNewRoomBtn />} />
-        <Route path='/ChooseNewRoom' element={<ChooseNewRoom />} />
+        <Route path='/ChooseNewRoom' element={<ChooseNewRoom addNewRoom={addNewRoom}/>} />
       </Routes>
     </div>
   );
--- newapp/src/Components/ChooseNewRoom.jsx
+++ newapp/src/Components/ChooseNewRoom.jsx
@@ -1,20 +1,25 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-export default function ChooseNewRoom() {
+export default function ChooseNewRoom({addNewRoom}) {
 
-    const [newRoom, setNewRoom] = useState('')
-    console.log(newRoom);
+    const nav = useNavigate()
+
+    const [chooseNewRoom, setChooseNewRoom] = useState('')
+    const[roomName,setRoomName]=useState('')
+    const[roomColor,setRoomColor]=useState('')
+
     return (
         <div>
-            <select onChange={(e) => { setNewRoom(e.target.value) }}>
-                <option value="" disabled hidden>Choose room</option>
+            <select value={chooseNewRoom} onChange={(e) => { setChooseNewRoom(e.target.value) }}>
+                <option value='' disabled hidden>Choose room</option>
                 <option value='kitchen'>kitchen</option>
                 <option value='bathroom'>bathroom</option>
                 <option value='bedroom'>bedroom</option>
             </select><br />
-            <input maxLength={5} type='text' placeholder='room name' /><br />
-            <input type='text' placeholder='room color' /><br />
-            <button>CREATE</button>
+            <input onChange={(e)=>{setRoomName(e.target.value)}} maxLength={5} type='text' placeholder='room name' /><br />
+            <input onChange={(e)=>{setRoomColor(e.target.value)}} type='text' placeholder='room color' /><br />
+            <button onClick={()=>{addNewRoom(chooseNewRoom,roomName,roomColor);nav('/')}}>CREATE</button>
         </div>
     )
 }
