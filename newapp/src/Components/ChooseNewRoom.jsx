8a56f2f7f74c0d33e572f066300a7ce81279674b
abgilad <120219883+abgilad@users.noreply.github.com>
2023-06-22 13:34:39 +0300

    22.06.23 third update

--- newapp/src/Components/ChooseNewRoom.jsx
+++ newapp/src/Components/ChooseNewRoom.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-export default function ChooseNewRoom({addNewRoom}) {
+export default function ChooseNewRoom({ addNewRoom }) {
 
     const nav = useNavigate()
 
+
     const [chooseNewRoom, setChooseNewRoom] = useState('')
-    const[roomName,setRoomName]=useState('')
-    const[roomColor,setRoomColor]=useState('')
+    const [roomName, setRoomName] = useState('')
+    const [roomColor, setRoomColor] = useState('')
 
     return (
         <div>
@@ -17,9 +18,9 @@ export default function ChooseNewRoom({addNewRoom}) {
                 <option value='bathroom'>bathroom</option>
                 <option value='bedroom'>bedroom</option>
             </select><br />
-            <input onChange={(e)=>{setRoomName(e.target.value)}} maxLength={5} type='text' placeholder='room name' /><br />
-            <input onChange={(e)=>{setRoomColor(e.target.value)}} type='text' placeholder='room color' /><br />
-            <button onClick={()=>{addNewRoom(chooseNewRoom,roomName,roomColor);nav('/')}}>CREATE</button>
+            <input onChange={(e) => { setRoomName(e.target.value) }} maxLength={5} type='text' placeholder='room name' /><br />
+            <input onChange={(e) => { setRoomColor(e.target.value) }} type='text' placeholder='room color' /><br />
+            <button onClick={() => { addNewRoom(chooseNewRoom, roomName, roomColor); nav('/') }}>CREATE</button>
         </div>
     )
 }
